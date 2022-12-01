bf5ab7f937e89036ea5a4df07043a5a695f680cc
Natnahom <natnahom12@gmail.com>
2022-12-01 14:43:40 +0300

    modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv)
+    result = 0
+
+    if num > 1
+        for i in range(1, num):
+            result += sys.argv[i]
+        print(result)
+    else:
+        print(result)
